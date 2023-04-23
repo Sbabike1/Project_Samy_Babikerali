3e7f7a7f77cf9e8e8a896e579f32be7e116eafed
Sbabike1 <129065917+Sbabike1@users.noreply.github.com>
2023-04-23 08:18:02 -0700

    Policy Class
    Added code for Policy class.

---
+++ Policy.java
@@ -0,0 +1,133 @@
+public class Policy
+{
+
+// Declaring Fields
+private int PolicyNumber;
+private String ProviderName;
+private String FirstName;
+private String LastName;
+private int Age;
+private String Smoker;
+private double Height;
+private double Weight;
+   
+// no arg constructor
+   public Policy()
+   {
+     PolicyNumber = 0;
+     ProviderName = "";
+     FirstName = "";
+     LastName = "";
+     Age = 0;
+     Smoker = "";
+     Height = 0.0;
+     Weight = 0.0; 
+   }
+   
+// constructor
+   public Policy(int n, String na, String f, String l, int a, String s, double h, double w)
+   {
+     PolicyNumber = n;
+     ProviderName = na;
+     FirstName = f;
+     LastName = l;
+     Age = a;
+     Smoker = s;
+     Height = h;
+     Weight = w; 
+   }
+   
+// setters
+   public void setPolicyNumber(int n)
+   {
+     PolicyNumber = n;
+   }
+   
+    public void setProviderName(String na)
+    {
+      ProviderName = na;
+    }
+    
+    public void setFirstName(String f)
+    {
+      FirstName = f;
+    }
+    
+    public void setLastName(String l)
+    {
+      LastName = l;
+    }
+    
+    public void setAge(int a)
+    {
+      Age = a;
+    }
+    
+    public void setSmoker(String s)
+    {
+      Smoker = s;
+    }
+    
+    public void setHeight(double h)
+    {
+      Height = h;
+    }
+    
+    public void setWeight(double w)
+    {
+      Weight = w;
+    }
+    
+// getters
+   public int getPolicyNumber()
+   {
+     return PolicyNumber;
+   }
+   
+    public String getProviderName()
+    {
+      return ProviderName;
+    }
+    
+    public String getFirstName()
+    {
+      return FirstName;
+    }
+    
+    public String getLastName()
+    {
+      return LastName;
+    }
+    
+    public int getAge()
+    {
+      return Age;
+    }
+    
+    public String getSmoker()
+    {
+      return Smoker;
+    }
+    
+    public double getHeight()
+    {
+      return Height;
+    }
+    
+    public double getWeight()
+    {
+      return Weight;  
+    }  
+    
+// calc methods
+   public double calcBmi()
+   {
+   return (Weight * 703)/(Height*Height);
+   }
+   
+   public double calcFee()
+   {
+   return (calcBmi() - 35)*20;
+   }
+
+}
--- Project_Samy_Babikerali.java
+++
@@ -1,6 +0,0 @@
-import java.util.Scanner;
-
-public class Project_Samy_Babikerali
-{
-   public static void main(String[] args)
-   {
